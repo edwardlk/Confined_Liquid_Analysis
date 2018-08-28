7f3dfa50d2bf2e9f18a4d593882c2993c5b66a71
Ed K <e.kramkowski@gmail.com>
2018-08-28 00:12:58 -0400

    new data import & changing to graph approach and retract

--- singleAnalysis.py
+++ singleAnalysis.py
@@ -6,14 +6,13 @@ import matplotlib.pyplot as plt
 from tkinter import Tk, filedialog
 
 from CLfuncs import Deflection, Movement, Amplitude, Phase, Stiffness, Damping
-from CLfuncs import Relaxation, smooth, graphMax  # , outputFiles
-
-# Designate data file to analyze.
-root = Tk()
-root.withdraw()
+from CLfuncs import Relaxation, smooth, graphMax, joinAR, joinAR2
 
 fileAsk = False
 if fileAsk:
+    # Designate data file to analyze.
+    root = Tk()
+    root.withdraw()
     info = 'Please select the data file you wish to analyze.'
     dataLoc = filedialog.askopenfilename(parent=root, initialdir="/",
                                          title=info)
@@ -23,8 +22,8 @@ if fileAsk:
     print("dataLoc =", dataLoc)
     print("conLoc =", conLoc)
 else:
-    dataLoc = 'C:/Users/ekram/Desktop/2018-08-20_LTAFM-Testing/temp-001.txt'
-    conLoc = 'C:/Users/ekram/Desktop/2018-08-20_LTAFM-Testing/constants.txt'
+    dataLoc = 'C:/Users/ekram/Desktop/2018-08-22_water/water-012.txt'
+    conLoc = 'C:/Users/ekram/Desktop/2018-08-22_water/constants.txt'
 
 dataFile = path.split(dataLoc)[1]
 conFile = path.split(conLoc)[1]
@@ -41,38 +40,34 @@ constants = np.genfromtxt(conLoc, skip_header=1)
 
 x = fileNum - 1
 
+# 0:Index 1:Distance(Ang) 2:Tunnel(nA) 3:Ipd(mV) 4:Extin(V) 5:ADC1(V) 6:ADC2(V)
+# 7:R_Tunnel(nA) 8:R_Ipd(mV) 9:R_Extin(V) 10:R_ADC1(V) 11:R_ADC2(V)
 data = np.genfromtxt(dataLoc, skip_header=20, skip_footer=1)
-
 rows = data.shape[0]
 columns = data.shape[1]
-
-Index = np.zeros(rows)      # Index
-Distance = np.zeros(rows)   # Distance
-Ipd = np.zeros(rows)        # Photo Diode Current
-Extin = np.zeros(rows)      # External Input
-ADC1 = np.zeros(rows)       # Spare ADC Channel 1
-ADC2 = np.zeros(rows)       # Spare ADC Channel 2
-
-for x1 in range(0, rows):
-    Index[x1] = data[x1, 0]
-    Distance[x1] = data[x1, 1]
-    Ipd[x1] = data[x1, 3]
-    Extin[x1] = data[x1, 4]
-    ADC1[x1] = data[x1, 5]
-    ADC2[x1] = data[x1, 6]
+(Index, Distance, Tunnel, Ipd, Extin, ADC1, ADC2, R_Tunnel, R_Ipd, R_Extin,
+    R_ADC1, R_ADC2) = data.T
 
 pos = np.zeros(rows)        # Actual Position (d+z)
 amp = np.zeros(rows)        # Amplitude
+R_amp = np.zeros(rows)
 phi = np.zeros(rows)        # Phase
+R_phi = np.zeros(rows)
 k_ts = np.zeros(rows)       # Interaction Stiffness
+R_k_ts = np.zeros(rows)
 gamma = np.zeros(rows)      # Damping Coefficient
+R_gamma = np.zeros(rows)
 t_R = np.zeros(rows)	    # Relaxation Time
+R_t_R = np.zeros(rows)
 k_tsavg = np.zeros(rows)    # Interaction Stiffness
 
 for x2 in range(0, rows):
     phi[x2] = Phase(ADC1[x2])
+    R_phi[x2] = Phase(R_ADC1[x2])
     amp[x2] = Amplitude(Extin[x2], constants[x, 2], constants[x, 3],
                         constants[x, 1])
+    R_amp[x2] = Amplitude(R_Extin[x2], constants[x, 2], constants[x, 3],
+                          constants[x, 1])
 
 phi0 = phi[0]  # changed to match shah analysis
 A0 = amp[8]
@@ -80,50 +75,64 @@ Amax = max(amp[:99])
 
 for x3 in range(0, rows):
     pos[x3] = Deflection(Ipd[x3], constants[x, 3]) + Movement(x3)
-    phi[x3] = Phase(ADC1[x3])
     k_ts[x3] = Stiffness(constants[x, 7], Amax, phi[x3], phi0, amp[x3])
+    R_k_ts[x3] = Stiffness(constants[x, 7], Amax, R_phi[x3], phi0, R_amp[x3])
     gamma[x3] = Damping(constants[x, 7], A0, phi[x3], phi0, amp[x3],
                         constants[x, 6])
+    R_gamma[x3] = Damping(constants[x, 7], A0, R_phi[x3], phi0, R_amp[x3],
+                          constants[x, 6])
 
 k_tsavg = smooth(k_ts, 11, 'hamming')
+R_k_tsavg = smooth(R_k_ts, 11, 'hamming')
 gammaavg = smooth(gamma, 11, 'hamming')
+R_gammaavg = smooth(R_gamma, 11, 'hamming')
 
 for x4 in range(0, rows):
     t_R[x4] = Relaxation(k_tsavg[x4], gammaavg[x4], constants[x, 6])
+    R_t_R[x4] = Relaxation(R_k_tsavg[x4], R_gammaavg[x4], constants[x, 6])
+
+ExtinAR, DistAR, x5 = joinAR(Extin, R_Extin, Distance)
+ADC1AR, Dist1AR = joinAR2(ADC1, R_ADC1, Distance, x5)
+k_tsAR, Dist2AR = joinAR2(k_tsavg, R_k_tsavg, Distance, x5)
+gammaavgAR, Dist3AR = joinAR2(gammaavg, R_gammaavg, Distance, x5)
+t_RAR, Dist4AR = joinAR2(t_R, R_t_R, Distance, x5)
 
 # PLOT CALCULATED VALUES
-
 fig = plt.figure(figsize=(6, 7))
+plt.subplots_adjust(top=0.93)
+
 ax1 = fig.add_subplot(311)
-ax1.plot(Distance, Extin, 'r.')
+plt.axvline(x=0)
+ax1.plot(DistAR, ExtinAR, 'r.-')
 # ax1.set_xlabel('Distance (Angstroms)')
 ax1.set_ylabel('Extin (V)', color='r')
 for tl in ax1.get_yticklabels():
     tl.set_color('r')
 
 ax2 = ax1.twinx()
-ax2.plot(Distance, ADC1, 'b.')
+ax2.plot(Dist1AR, ADC1AR, 'b.-')
 ax2.set_ylabel('ADC Ch 2 (V)', color='b')
 for tl in ax2.get_yticklabels():
     tl.set_color('b')
 
 ax3 = fig.add_subplot(312)
-ax3.plot(Distance, k_tsavg, 'r.')
-ax3.set_xlabel('Distance (Angstroms)')
+plt.axvline(x=0)
+ax3.plot(Dist2AR, k_tsAR, 'r.-')
+# ax3.set_xlabel('Distance (Angstroms)')
 ax3.set_ylabel('Stiffness', color='r')
 ax3.set_ylim([0, graphMax(k_ts, 12)])
 for tl in ax3.get_yticklabels():
     tl.set_color('r')
 
 ax4 = ax3.twinx()
-ax4.plot(Distance, gammaavg, 'b.')
+ax4.plot(Dist3AR, gammaavgAR, 'b.-')
 ax4.set_ylabel('Damping Coefficient', color='b')
 ax4.set_ylim([0, graphMax(gammaavg, 0.002)])
 for tl in ax4.get_yticklabels():
     tl.set_color('b')
 
 ax5 = fig.add_subplot(313)
-ax5.plot(Distance, k_tsavg, 'r.')
+ax5.plot(Dist2AR, k_tsAR, 'r.-')
 ax5.set_xlabel('Distance (Angstroms)')
 ax5.set_ylabel('Stiffness', color='r')
 ax5.set_ylim([0, graphMax(k_tsavg, 12)])
@@ -131,7 +140,7 @@ for tl in ax5.get_yticklabels():
     tl.set_color('r')
 
 ax6 = ax5.twinx()
-ax6.plot(Distance, t_R, 'b.')
+ax6.plot(Dist4AR, t_RAR, 'b.-')
 ax6.set_ylabel('Relaxation Time', color='b')
 ax6.set_ylim([0, 0.010])
 for tl in ax6.get_yticklabels():
@@ -144,3 +153,5 @@ plt.suptitle(
 
 plt.show()
 plt.close('all')
+
+print('Done')
