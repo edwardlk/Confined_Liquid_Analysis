400440c5b43e41cbb723a51ae475cb92c992174e
edwardlk <e.kramkowski@gmail.com>
2019-02-20 13:04:44 -0500

    finished(?) xlsx creator

--- excelSheetCreator.py
+++ excelSheetCreator.py
@@ -5,6 +5,12 @@ from shutil import copyfile
 from os import path, listdir, makedirs
 from tkinter import Tk, filedialog
 
+csvHeader2 = ('Index', 'Distance(Ang)', 'Tunnel(nA)', 'Ipd(mV)', 'Extin(V)',
+              'ADC1(V)', 'ADC2(V)', 'ADC3(V)', 'ADC4(V)', 'ADC5(V)', 'ADC6(V)',
+              'ADC7(V)', 'ADC8(V)', 'RTunnel(nA)', 'RIpd(mV)', 'RExtin(V)',
+              'RADC1(V)', 'RADC2(V)', 'RADC3(V)', 'RADC4(V)', 'RADC5(V)',
+              'RADC6(V)', 'RADC7(V)', 'RADC8(V)')
+
 root = Tk()
 root.withdraw()
 
@@ -13,41 +19,40 @@ info = ('Please select the folder that contains the data to convert to xlsx.')
 srcDir = filedialog.askdirectory(parent=root, initialdir="/", title=info)
 dstDir = path.join(srcDir, 'xlsx-output')
 
+if not path.exists(dstDir):
+    makedirs(dstDir)
+
+info2 = 'Select the file that contains the constants.'
+conLoc = filedialog.askopenfilename(parent=root, initialdir=srcDir,
+                                    title=info2)
+constants = pd.read_csv(conLoc, sep='\t', header=0)
+
 # Get file list from source directory
 dataFiles = listdir(srcDir)
 dataFiles.sort()
 
-dataFile = 'D:/ekram/Desktop/2019-02-13/009.txt'
-data = np.genfromtxt(dataFile, skip_header=21, skip_footer=1)
+for x in range(len(dataFiles)):
+    dataFile = path.join(srcDir, dataFiles[x])
+    data = np.genfromtxt(dataFile, skip_header=21, skip_footer=1)
 
-csvHeader2 = (
-    'Index','Distance(Ang)','Tunnel(nA)','Ipd(mV)','Extin(V)','ADC1(V)',
-    'ADC2(V)','ADC3(V)','ADC4(V)','ADC5(V)','ADC6(V)','ADC7(V)','ADC8(V)',
-    'RTunnel(nA)','RIpd(mV)','RExtin(V)','RADC1(V)','RADC2(V)','RADC3(V)',
-    'RADC4(V)','RADC5(V)','RADC6(V)','RADC7(V)','RADC8(V)')
+    df = pd.DataFrame(data=data, columns=csvHeader2)
 
-df = pd.DataFrame(data=data, columns=csvHeader2)
+    # constOut = constants[x:(x+1)].transpose()
+    constOut = constants.iloc[x].transpose()
+    excelBookName = (str(constants.loc[x, 'Speed(A/s)']) + 'Aps-LTAFM-'
+                     + str(x).zfill(3) + '.xlsx')
+    excelBookLoc = path.join(dstDir, excelBookName)
 
-constants = pd.read_csv('D:/ekram/Desktop/2019-02-13/constants.txt', sep='\t',
-                        header=0)
+    # Put xlsx model file in github folder?
+    copyfile('___Aps-LTAFM-000v4.xlsx', excelBookLoc)
 
-constOut = constants[9:10].transpose()
+    book = load_workbook(excelBookLoc)
+    writer = pd.ExcelWriter(excelBookLoc, engine='openpyxl', mode='a')
 
-print(constants)
-print(constants[2:3])
+    writer.book = book
+    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
 
-excelBookName = (str(constants.loc[(9-1), 'Speed(A/s)']) + 'Aps-LTAFM-'
-                 + str('009.xlsx'))
+    df.to_excel(writer, sheet_name='raw', index=False)
+    constOut.to_excel(writer, sheet_name='Approach', header=False, startrow=1)
 
-copyfile('___Aps-LTAFM-000v4.xlsx', excelBookName)
-
-book = load_workbook(excelBookName)
-writer = pd.ExcelWriter(excelBookName, engine='openpyxl', mode='a')
-
-writer.book = book
-writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
-
-df.to_excel(writer, sheet_name='raw', index=False)
-constOut.to_excel(writer, sheet_name='Approach', header=False, startrow=1)
-
-writer.save()
+    writer.save()
